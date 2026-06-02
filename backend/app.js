8ac1b34c92152a53773c13f4886e208edee662a4
Aman <aman.sahni052001@gmail.com>
2026-06-02 07:27:05 +0530

    working todo app with frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  next();
+});
+
 app.use(express.json());
 
 let todos = [];
